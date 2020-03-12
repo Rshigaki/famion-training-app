ebdefd8aa4ab1dcbf3ab3e0f0926d55776c31edf
r.shigaki <ryusei_shigaki@en-konkatsu.com>
2020-03-13 01:37:43 +0900

    ログイン画面まで

--- Gemfile
+++ Gemfile
@@ -31,6 +31,9 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem 'devise'
 gem 'slim'
 gem 'html2slim'
+gem 'bootstrap'
+gem 'jquery-rails'
+gem 'rails-i18n'
 
 
 group :development, :test do
@@ -45,6 +48,8 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'letter_opener'
+  gem 'letter_opener_web'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, :controllers => {
+      :registrations => 'users/registrations',
+      :sessions => 'users/sessions'
+  }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+  end
+  
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  end
+  
+  scope '/', module: :public do
+    root to: 'top#index'
+  end
 end

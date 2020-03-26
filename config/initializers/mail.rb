72f606265420b605b23f575b18698e5bf7bcdad6
r.shigaki <ryusei_shigaki@en-konkatsu.com>
2020-03-27 03:50:36 +0900

    mailgun設定

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,5 @@
 if Rails.env.production?
   ActionMailer::Base.delivery_method = :mailgun
-  ActionMailer::Base.mailgun_settings = {
-      api_key: ENV['MAILGUN_API_KEY'],
-      domain: ENV['MAILGUN_DOMAIN']
-  }
 elsif Rails.env.development?
   ActionMailer::Base.delivery_method = :letter_opener
 else
